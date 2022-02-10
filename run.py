d2c12245f644bf7e573306cf26a17c2c5020e623
Andre Nascimento Maia <amaia@uolinc.com>
2022-02-09 21:02:27 -0300

    Fix FLASK_ENV

--- run.py
+++ run.py
@@ -3,10 +3,8 @@ from api import create_app
 from api.model import initialize_db
 import os
 
-
 flask_env = os.getenv('FLASK_ENV')
-
-print('FLASK_ENV:' + flask_env)
+print('FLASK_ENV:' + str(flask_env))
 
 app = create_app(flask_env)
 app.app_context().push()
